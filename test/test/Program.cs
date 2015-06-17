e2c4b4227a89dfeba2cc6aaee17a7b09fdade597
Nechitaylo <dicoy@meta.ua>
2015-06-18 02:42:45 +0300

    second commit

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -10,6 +10,15 @@ namespace test
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 20;
+            int c;
+            int z;
+            c = b + a;
+            z = b * a;
+            Console.WriteLine("c=" + c);
+            Console.WriteLine("z=" + z);
+            Console.ReadKey();
         }
     }
 }
